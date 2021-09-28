73b77262b8b9fb4af4a69c6844d252f5c7d9acf1
Quốc Thắng <comehere.thang@gmail.com>
2021-09-28 21:03:01 +0700

    Update main.js

--- contact/assets/js/main.js
+++ contact/assets/js/main.js
@@ -5,7 +5,7 @@ function postToGoogle() {
 	var mess = $("#messField").val();
 
 	$.ajax({
-		url: "https://docs.google.com/forms/d/e/1FAIpQLSeB5bi_fdazbbikDELKuWHoNdP-CrqT91ncghlc3NNnx9hq5w/formResponse?",
+		url: "https://docs.google.com/forms/d/e/1FAIpQLSewuOZr0deyFAagCMWyTvZxJ8oWWwyQ-hXiuGyYe7zYCb9S8Q/formResponse?",
 		data: {"entry.896447091": name, "emailAddress": email, "entry.1996542779": mess},
 		type: "POST",
 		dataType: "jsonp",
